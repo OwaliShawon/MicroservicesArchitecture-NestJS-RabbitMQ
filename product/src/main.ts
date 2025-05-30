e9355b5f1bbd4181ba68d15c9b8b19d8e92d53c3
OwaliShawon <owali.shawon@gmail.com>
2025-05-30 18:51:28 +0600

    Chore (product) as microservice

--- product/src/main.ts
+++ product/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  const rabbitMqUrl = configService.get<string>('RABBITMQ_URL');
+  const rabbitMqAuthQueue = configService.get<string>(
+    'RABBITMQ_USER_EVENTS_QUEUE',
+  );
+  if (!port || !rabbitMqUrl || !rabbitMqAuthQueue) {
+    throw new Error('Missing required environment variables');
+  }
+
   await app.listen(3001);
+
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rabbitMqUrl],
+        queue: rabbitMqAuthQueue,
+        queueOptions: {
+          durable: false,
+        },
+      },
+    });
+  await microservice.listen();
 }
 bootstrap();
