2530d77519009a3307b49f2eefd6f37d0d1db2f6
OwaliShawon <owali.shawon@gmail.com>
2025-05-31 14:29:33 +0600

    Add (auth) swagger

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -12,24 +12,54 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { ApiTags, ApiBody, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiBody({
+    type: RegisterAuthDto,
+    examples: {
+      example: {
+        summary: 'Register Example',
+        value: {
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'strongPassword123',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'User registered successfully.' })
   async register(@Body() registerDto: RegisterAuthDto) {
     return this.authService.register(registerDto);
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiBody({
+    type: LoginAuthDto,
+    examples: {
+      example: {
+        summary: 'Login Example',
+        value: {
+          email: 'john@example.com',
+          password: 'strongPassword123',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 200, description: 'User logged in successfully.' })
   async login(@Body() loginDto: LoginAuthDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('refresh')
   @UseGuards(AuthGuard('jwt-refresh'))
+  @ApiResponse({ status: 200, description: 'Token refreshed successfully.' })
   async refresh(@Req() req: any) {
     const payload = {
       userId: req.user.userId,
@@ -44,6 +74,14 @@ export class AuthController {
 
   @Post('logout')
   @HttpCode(HttpStatus.OK)
+  @ApiBody({
+    schema: {
+      example: {
+        refreshToken: 'your-refresh-token-here',
+      },
+    },
+  })
+  @ApiResponse({ status: 200, description: 'User logged out successfully.' })
   async logout(@Body() { refreshToken }: { refreshToken: string }) {
     return this.authService.logout(refreshToken);
   }
--- auth/src/main.ts
+++ auth/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
   if (!port || !rabbitMqUrl || !rabbitMqAuthQueue) {
     throw new Error('Missing required environment variables');
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Service API')
+    .setDescription('API documentation for the Auth Service')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(port);
 
