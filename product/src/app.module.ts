25851f74121f1ba973b282df035f2fab40158f36
OwaliShawon <owali.shawon@gmail.com>
2025-05-31 17:02:45 +0600

    Add (product) db connection

--- product/src/app.module.ts
+++ product/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- product/src/config/configuration.ts
+++ product/src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3001,
   database: {
-    url: process.env.DATABASE_URL || 'mongodb://localhost:27017/product-db',
+    uri: process.env.DATABASE_URL || 'mongodb://localhost:27017/product-db',
   },
 });
