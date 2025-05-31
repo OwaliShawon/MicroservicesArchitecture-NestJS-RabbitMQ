358725529a4a1e890a418d77cc75a013104e0d7e
OwaliShawon <owali.shawon@gmail.com>
2025-05-31 16:45:53 +0600

    Add (product) swagger config

--- product/src/main.ts
+++ product/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,14 @@ async function bootstrap() {
   if (!port || !rabbitMqUrl || !rabbitMqAuthQueue) {
     throw new Error('Missing required environment variables');
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('Product Service API')
+    .setDescription('API documentation for the Product Service')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3001);
 
